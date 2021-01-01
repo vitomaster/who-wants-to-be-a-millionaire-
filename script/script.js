44111450d29725f005c20457d7b4fa3723bbefc8
vitomaster <63310587+vitomaster@users.noreply.github.com>
2021-01-01 03:28:08 +0200

    update

--- script/script.js
+++ script/script.js
@@ -7,85 +7,332 @@ const contentContainer = document.querySelector('.contentContainer');
 let questionTextBox = document.querySelector('.questionTextBox');
 const btn = document.querySelectorAll('.btn');
 
-const questions = {
-    questionText1:"Скока будет 2+2?",
-    questionText2:'Кто был 44-ым президентом США?',
-    questionText3:'Один в поле не воин, a...'
-};
-
-class x {
-    constructor(question,anwsers){
-        this.question = question;
-        this.awnsers = anwsers;
-    }
-    questionCreator(){
-        questionTextBox.textContent = this.question;
-        
-        const anwsersShow = this.anwsers;
-        for(let i = 0; i < btn.length; i++){
-            btn[i].textContent = anwsersShow;
-        }
-    }
+const btns = {
+    a:btnA,
+    b:btnB,
+    c:btnC,
+    d:btnD
 }
 
-const anwsersList = {
+const questions = {
+    questionWrong:'Вы ответили не верно! Играть ещё раз',
+    questionText1:"Скока будет 2+2?",
+    questionText2:'Кто был 44-ым президентом США?',
+    questionText3:'Где, если верить пословице, любопытной Варваре нос оторвали?',
+    questionText4:'Сколько на Земле континентов?',
+    questionText5:'Что такое Тигр и Евфрат?',
+    questionText6:'Кто размещает в интернете грубые или провокационные сообщения?',
+    questionText7:"На территории какого современного государства находится легендарная Троя?",
+    questionText8:'Кpyпнoe быcтpoxoднoe паccажиpcкoe cyднo',
+    questionText9:'Чем заряжали пушки для уничтожения пехоты?',
+    questionText10:'Из чего не состоит атом?',
+};
+
+
+const anwsersList1 = {
     a:"3",
     b:"5",
-    c:"4",
-    d:"0",
-    readAnwsers:() => {
-        for (let key in anwsersList){
-            console.log(anwsersList[key]);
-        }
-    }
+    c:"4", //true
+    d:"0"
 };
 
 const anwsersList2 = {
-    a:'Барак Обама',
+    a:'Барак Обама', //true
     b:"Билл Клинтон",
     c:"Ричард Никсон",
     d:"Владимир Путин"
 }
 
+const anwsersList3 = {
+    a:'В магазине',
+    b:'На базаре',//true
+    c:'На работе', 
+    d:'На море'
+}
 
+const anwsersList4 = {
+    a:'7',
+    c:'6', //true
+    b:'8',
+    d:'1'
+}
+
+const anwsersList5 = {
+    a:"Моря",
+    b:"Озёра",
+    c:"Животные", 
+    d:"Реки",//true
+}
+
+const anwsersList6 = {
+    a:'Тролли',//true
+    b:'Хакеры',
+    c:'Грубые провокаторы',
+    d:'Маркетологи'
+}
+
+const anwsersList7 = {
+    a:"Египет",
+    b:"Ливия",
+    c:"Греция",
+    d:"Турция", //true
+}
+
+const anwsersList8 = {
+    a:"Круизное судно",
+    c:"Нэрроубот",
+    b:"Лайнер",//true
+    d:"Турбоход",
+}
+const anwsersList9 = {
+    a:"Картечь",//true
+    b:"Шрапнель",
+    c:"Сняряд",
+    d:"Ядро",
+}
+
+const anwsersList10 = {
+    a:"Фотон",//true
+    b:"Протон",
+    c:"Нейтрон",
+    d:"Электрон",
+}
 
 contentContainer.addEventListener('click', startGame);
 
 function startGame(){
     contentContainer.removeEventListener('click', startGame);
-    let showMeQuestion = new x(questions.questionText1 ,anwsersList.readAnwsers);
-    showMeQuestion.questionCreator();
-    btnB.addEventListener('click', questionN2);
+    questionN1();
 }
 
-// function questionN1(){
-//     questionTextBox.textContent = questions.questionText1;
-//     const anwsers = Object.values(anwsersList1);
-//         for(let i = 0; i < btn.length; i++){
-//             btn[i].textContent = anwsers[i];
-//         }
-// }
+
+
+function questionN1(){
+
+    questionTextBox.textContent = questions.questionText1;
+
+    const anwsers = Object.values(anwsersList1);
+
+    for(let i = 0; i < btn.length; i++){
+        btn[i].textContent = anwsers[i];
+    }
+
+    for (let key in btns){
+        btns[key].addEventListener('click', function(){
+            if(key == 'a' || key == 'd' || key == 'c'){
+                questionTextBox.textContent = questions.questionWrong;
+                contentContainer.addEventListener('click', startGame);
+            }
+            if(key == 'b'){
+                questionN2();
+            }
+        })
+    }
+}
 
 function questionN2(){
     questionTextBox.textContent = questions.questionText2;
+
     const anwsers = Object.values(anwsersList2);
+
+    for(let i = 0; i < btn.length; i++){
+        btn[i].textContent = anwsers[i];
+    }
+
+    for (let key in btns){
+        btns[key].addEventListener('click', function(){
+            if(key == 'b' || key == 'd' || key == 'c'){
+                questionTextBox.textContent = questions.questionWrong;
+                contentContainer.addEventListener('click', startGame);
+            }
+            if(key == 'a'){
+                questionN3();
+            }
+        })
+    }
+}
+
+function questionN3 (){
+
+    questionTextBox.textContent = questions.questionText3;
+
+    const anwsers = Object.values(anwsersList3);
+
+    for(let i = 0; i < btn.length; i++){
+        btn[i].textContent = anwsers[i];
+    }
+
+    for (let key in btns){
+        btns[key].addEventListener('click', function(){
+            if(key == 'b' || key == 'd' || key == 'a'){
+                questionTextBox.textContent = questions.questionWrong;
+                contentContainer.addEventListener('click', startGame);
+            }
+            if(key == 'c'){
+                questionN4();
+            }
+        })
+    }
+}
+
+function questionN4(){
+    
+    questionTextBox.textContent = questions.questionText4;
+
+    const anwsers = Object.values(anwsersList4);
+
+    for(let i = 0; i < btn.length; i++){
+        btn[i].textContent = anwsers[i];
+    }
+
+    for (let key in btns){
+        btns[key].addEventListener('click', function(){
+            if(key == 'b' || key == 'd' || key == 'a'){
+                questionTextBox.textContent = questions.questionWrong;
+                contentContainer.addEventListener('click', startGame);
+            }
+            if(key == 'c'){
+                questionN5();
+            }
+        })
+    }
+}
+
+function questionN5(){
+
+    questionTextBox.textContent = questions.questionText5;
+
+    const anwsers = Object.values(anwsersList5);
+
         for(let i = 0; i < btn.length; i++){
             btn[i].textContent = anwsers[i];
         }
+        for (let key in btns){
+            btns[key].addEventListener('click', function(){
+                if(key == 'b' || key == 'c' || key == 'a'){
+                    questionTextBox.textContent = questions.questionWrong;
+                    contentContainer.addEventListener('click', startGame);
+                }
+                if(key == 'd'){
+                    questionN6();
+                }
+            })
+        }
 }
 
+function questionN6(){
 
+    questionTextBox.textContent = questions.questionText6;
 
-// function questionN(){
-//     questionTextBox.textContent = x.questionText;
-//     const anwsers = Object.values(y);
+    const anwsers = Object.values(anwsersList6);
+
+        for(let i = 0; i < btn.length; i++){
+            btn[i].textContent = anwsers[i];
+        }
+        for (let key in btns){
+            btns[key].addEventListener('click', function(){
+                if(key == 'b' || key == 'c' || key == 'd'){
+                    questionTextBox.textContent = questions.questionWrong;
+                    contentContainer.addEventListener('click', startGame);
+                }
+                if(key == 'a'){
+                    questionN7();
+                }
+            })
+        }
+}
+
+function questionN7(){
+
+    questionTextBox.textContent = questions.questionText7;
+
+    const anwsers = Object.values(anwsersList7);
+
+        for(let i = 0; i < btn.length; i++){
+            btn[i].textContent = anwsers[i];
+        }
+        for (let key in btns){
+            btns[key].addEventListener('click', function(){
+                if(key == 'b' || key == 'c' || key == 'a'){
+                    questionTextBox.textContent = questions.questionWrong;
+                    contentContainer.addEventListener('click', startGame);
+                }
+                if(key == 'd'){
+                    questionN8();
+                }
+            })
+        }
+}
+
+function questionN8(){
+    questionTextBox.textContent = questions.questionText8;
+    const anwsers = Object.values(anwsersList8);
+        for(let i = 0; i < btn.length; i++){
+            btn[i].textContent = anwsers[i];
+        }
+        for (let key in btns){
+            btns[key].addEventListener('click', function(){
+                if(key == 'd' || key == 'c' || key == 'a'){
+                    questionTextBox.textContent = questions.questionWrong;
+                    contentContainer.addEventListener('click', startGame);
+                }
+                if(key == 'b'){
+                    questionN9();
+                }
+            })
+        }
+}
+
+function questionN9(){
+    questionTextBox.textContent = questions.questionText9;
+    const anwsers = Object.values(anwsersList9);
+        for(let i = 0; i < btn.length; i++){
+            btn[i].textContent = anwsers[i];
+        }
+        for (let key in btns){
+            btns[key].addEventListener('click', function(){
+                if(key == 'b' || key == 'c' || key == 'd'){
+                    questionTextBox.textContent = questions.questionWrong;
+                    contentContainer.addEventListener('click', startGame);
+                }
+                if(key == 'a'){
+                    questionN10();
+                }
+            })
+        }
+}
+
+function questionN10(){
+    questionTextBox.textContent = questions.questionText10;
+    const anwsers = Object.values(anwsersList10);
+        for(let i = 0; i < btn.length; i++){
+            btn[i].textContent = anwsers[i];
+        }
+        for (let key in btns){
+            btns[key].addEventListener('click', function(){
+                if(key == 'b' || key == 'c' || key == 'd'){
+                    questionTextBox.textContent = questions.questionWrong;
+                    contentContainer.addEventListener('click', startGame);
+                }
+                if(key == 'a'){
+                    questionTextBox.textContent = "Победа! С играть ещё раз"
+                    contentContainer.addEventListener('click', startGame);
+                }
+            })
+        }
+}
+
+// class x {
+//     constructor(question,anwsers){
+//         this.question = question;
+//         this.awnsers = anwsers;
+//     }
+//     questionCreator(){
+//         questionTextBox.textContent = this.question;
+        
+//         const anwsersShow = this.anwsers;
 //         for(let i = 0; i < btn.length; i++){
-//             btn[i].textContent = anwsers[i];
+//             btn[i].textContent = anwsersShow;
 //         }
+//     }
 // }
-
-//  btn.forEach(item =>{
-//         item.addEventListener('click',()=>{
-//             console.log('true');
-//         })
-//     })
